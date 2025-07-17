4d55494e0161eb454ed6a8da77724d846ae0e191
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-07-17 16:22:52 +0530

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <img src=''
+    </div>
+  )
+}
+
+export default page
